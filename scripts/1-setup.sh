a25805df77657bce7d0cfe185765283cf94f9769
justin <98235360+jcampbell4485@users.noreply.github.com>
2022-06-01 22:10:22 -0700

    change from grub to refind

--- scripts/1-setup.sh
+++ scripts/1-setup.sh
@@ -30,7 +30,7 @@ echo -ne "
 -------------------------------------------------------------------------
 "
 pacman -S --noconfirm --needed pacman-contrib curl
-pacman -S --noconfirm --needed reflector rsync grub arch-install-scripts git
+pacman -S --noconfirm --needed reflector rsync refind arch-install-scripts git
 cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.bak
 
 nc=$(grep -c ^processor /proc/cpuinfo)

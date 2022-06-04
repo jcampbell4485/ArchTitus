cd8d77308db97b5f3088250d502fa5efd76d4a29
justin <98235360+jcampbell4485@users.noreply.github.com>
2022-06-04 16:49:20 -0700

    revise line 159 for fstab file

--- scripts/0-preinstall.sh
+++ scripts/0-preinstall.sh
@@ -156,7 +156,7 @@ echo "keyserver hkp://keyserver.ubuntu.com" >> /mnt/etc/pacman.d/gnupg/gpg.conf
 cp -R ${SCRIPT_DIR} /mnt/root/ArchTitus
 cp /etc/pacman.d/mirrorlist /mnt/etc/pacman.d/mirrorlist
 
-genfstab -L /mnt >> /mnt/etc/fstab
+genfstab -U /mnt >> /mnt/etc/fstab #genfstab -L /mnt >> /mnt/etc/fstab
 echo " 
   Generated /etc/fstab:
 "

21bf386594a1d9e911b9aacf11a92c333f6d5ca0
justin <98235360+jcampbell4485@users.noreply.github.com>
2022-06-05 11:53:38 -0700

    Update 3-post-setup.sh

--- scripts/3-post-setup.sh
+++ scripts/3-post-setup.sh
@@ -33,7 +33,9 @@ echo -ne "
 #make and edit refind for arch linux and hard drive
 mkrlconf
 sed '/archisobasedir=arch/d/' /boot/refind_linux.conf
-sed 's/^root=PARTUUID/root=/dev/nvme0n1p2 rw add_efi_memmap" # /' /boot/EFI/BOOT/refind.conf
+sed 's|.*PARTUUID.*|/dev/nvme0n1p2 rw add_efi_memmap" # |' /boot/EFI/BOOT/refind.conf
+
+#sed 's/^root=PARTUUID/root=/dev/nvme0n1p2 rw add_efi_memmap" # /' /boot/EFI/BOOT/refind.conf
 
 #sed -i '/root=PARTUUID/c\   options   "root=/dev/nvme0n1p2 rw add_efi_memmap"' /boot/EFI/BOOT/refind.conf
 #sed 's/.*TEXT_TO_BE_REPLACED.*/This line is removed by the admin./'
